5beb92f256f158f44a70eefb066593af78786bae
劉育志 <38623851+thenamecouldbeLiu@users.noreply.github.com>
2018-05-30 00:21:49 +0800

    Add files via upload
    加上註解

--- ProblemSets and Practice/自我練習/PTT爬蟲.py
+++ ProblemSets and Practice/自我練習/PTT爬蟲.py
@@ -5,13 +5,14 @@ Created on Thu May 24 09:36:24 2018
 @author: Asus
 """
 #PTT爬蟲練習
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 import re
 import requests
 
 def mainfunction(Current_Soup,author_Name =None, key_Word=None):
 #finding author keyword or both
 #    regular_expression = re.compile()
+#    印出要求的keyword或是作者名稱
     if key_Word!=None and author_Name==None:
         for item in Current_Soup:
             if key_Word in item.select(".title")[0].text:
@@ -35,20 +36,27 @@ def Crawler(start_Url, author_Name =None, key_Word=None, Max_Number_Of_Pages= No
     else:
         try:
             if Max_Number_Of_Pages == None:
+#                未設定上限頁數
                 while True:
-                    Request_Current_Url = requests.get(Current_Url)
-                    next_Url ="https://www.ptt.cc" +bs(Request_Current_Url.text,"lxml").select("div.btn-group-paging a")[1]["href"]
-                    Current_Soup = bs(Request_Current_Url.text,"lxml").select(".r-ent")
+                    Request_Current_Url = requests.get(Current_Url)#解析網址
+                    next_Url ="https://www.ptt.cc" +BeautifulSoup(Request_Current_Url.text,"lxml").select("div.btn-group-paging a")[1]["href"]
+#                    抓取下一頁的TAG
+                    Current_Soup = BeautifulSoup(Request_Current_Url.text,"lxml").select(".r-ent")
+#                    用BeautifulSoup選所要元素
                     mainfunction(Current_Soup,author_Name, key_Word)
+#                    用mainfunction找keyword跟author
                     Current_Url = next_Url
+#                    把現在網址替換到下一頁
             else:
+#                設定上限頁數
                 for i in  range(Max_Number_Of_Pages):
                     Request_Current_Url = requests.get(Current_Url)
-                    next_Url ="https://www.ptt.cc" +bs(Request_Current_Url.text,"lxml").select("div.btn-group-paging a")[1]["href"]
-                    Current_Soup = bs(Request_Current_Url.text,"lxml").select(".r-ent")
+                    next_Url ="https://www.ptt.cc" +BeautifulSoup(Request_Current_Url.text,"lxml").select("div.btn-group-paging a")[1]["href"]
+#                    Current_Soup = BeautifulSoup(Request_Current_Url.text,"lxml").select(".r-ent")
                     mainfunction(Current_Soup,author_Name, key_Word)
                     Current_Url = next_Url
         except KeyError:
+#            有錯誤就跳過，到最後一個會跳到fianlly印出結束
             pass
         
         finally:
